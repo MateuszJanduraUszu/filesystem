7e29eb1b7235664bbce19b0a7fe6ad2b4a6d6c05
Mati4 <Mati4321@interia.pl>
2021-06-14 00:03:58 +0200

    Disable warning C6385 in <create_remove.cpp>, implement create_junction() and remove_all().

--- filesystem/filesystem/create_remove.cpp
+++ filesystem/filesystem/create_remove.cpp
@@ -9,6 +9,8 @@
 #if !_HAS_WINDOWS
 #pragma message("The contents of <create_remove.cpp> are available only with Windows 10.")
 #else // ^^^ !_HAS_WINDOWS ^^^ / vvv _HAS_WINDOWS vvv
+#pragma warning(push)
+#pragma warning(disable : 6385) // C6385: reading incorrect data
 
 _FILESYSTEM_BEGIN
 _EXPERIMENTAL_BEGIN
@@ -56,6 +58,52 @@ _NODISCARD bool __cdecl create_hard_link(const path& _To, const path& _Hardlink)
     return _Result;
 }
 
+// FUNCTION create_junction
+_NODISCARD bool __cdecl create_junction(const path& _To, const path& _Junction) {
+    if (!exists(_To)) { // directory not found
+        _Throw_fs_error("directory not found", error_type::runtime_error, "create_junction");
+    }
+
+    // at the beginning _Junction must be created as default directory 
+    create_directory(_Junction);
+
+    const HANDLE _Handle = _CSTD CreateFileW(_Junction.generic_wstring().c_str(),
+        static_cast<DWORD>(file_access::readonly | file_access::writeonly), 0, nullptr,
+        static_cast<DWORD>(file_disposition::only_if_exists), static_cast<DWORD>(
+            file_flags::backup_semantics | file_flags::open_reparse_point), nullptr);
+
+    if (_Handle == INVALID_HANDLE_VALUE) { // failed to get handle
+        _Throw_fs_error("failed to get handle", error_type::runtime_error, "create_junction");
+    }
+
+    unsigned char _Buff[sizeof(reparse_data_buffer) + MAX_PATH * sizeof(wchar_t)];
+    reparse_mountpoint_data_buffer& _Reparse_buff = reinterpret_cast<reparse_mountpoint_data_buffer&>(_Buff);
+
+    // set informations about reparse point
+    _CSTD memset(_Buff, 0, sizeof(_Buff));
+
+    // _Junction must containts prefix ("\??\") and sufix ("\"), to create junction successfully
+    _Reparse_buff._Reparse_tag                   = static_cast<unsigned long>(file_reparse_tag::mount_point);
+    int _Len                                     = _CSTD MultiByteToWideChar(static_cast<UINT>(code_page::acp), 0,
+        path(R"(\??\)" + _To.generic_string() + R"(\)").generic_string().c_str(), -1, _Reparse_buff._Reparse_target, MAX_PATH);
+    _Reparse_buff._Reparse_target_maximum_length = static_cast<uint16_t>((_Len--) * sizeof(wchar_t));
+    _Reparse_buff._Reparse_target_length         = static_cast<uint16_t>(_Len * sizeof(wchar_t));
+    _Reparse_buff._Reparse_data_length           = _Reparse_buff._Reparse_target_length + 12;
+
+    // attach reparse point to _Junction
+    unsigned long _Returned; // returned bytes
+    _CSTD DeviceIoControl(_Handle, FSCTL_SET_REPARSE_POINT, &_Reparse_buff,
+        _Reparse_buff._Reparse_data_length + 8, nullptr, 0, &_Returned, nullptr);
+    _CSTD CloseHandle(_Handle);
+
+    if (!is_junction(_Junction)) { // failed to create junction
+        _Throw_fs_error("failed to create junction", error_type::runtime_error, "create_junction");
+    }
+
+    // if won't throw an exception, will be able to return true
+    return true;
+}
+
 // FUNCTION create_symlink
 _NODISCARD bool __cdecl create_symlink(const path& _To, const path& _Symlink, const symlink_flags _Flags) {
     const bool _Result = _CSTD CreateSymbolicLinkW(_Symlink.generic_wstring().c_str(),
@@ -88,12 +136,56 @@ _NODISCARD bool __cdecl remove(const path& _Path) { // removes files and directo
 }
 
 // FUNCTION remove_all
-_NODISCARD bool __cdecl remove_all(const path& _Path, size_t _Count) {
-    return false;
+_NODISCARD bool __cdecl remove_all(const path& _Path) {
+    if (!is_directory(_Path) && !PathIsDirectoryW(_Path.generic_wstring().c_str())) { // directory not found
+        _Throw_fs_error("directory not found", error_type::runtime_error, "remove_all");
+    }
+
+    if (is_empty(_Path)) { // if empty, remove _Path and don't do anything else
+        return remove(_Path);
+    }
+
+    const auto _All = directory_data(_Path).total(); // total count of files, directories and others in _Path
+
+    for (const auto& _Elem : _All) { // remove one by one
+        remove(_Path + R"(\)" + _Elem); // requires full path
+    }
+
+    // remove this directory as well and don't check remove result, because remove() will check it
+    return remove(_Path);
 }
 
-_NODISCARD bool __cdecl remove_all(const path& _Path) {
-    return false;
+// FUNCTION remove_junction
+_NODISCARD bool __cdecl remove_junction(const path& _Target) {
+    if (!is_junction(_Target)) { // _Target must be a junction
+        _Throw_fs_error("expected junction", error_type::runtime_error, "remove_junction");
+    }
+
+    const HANDLE _Handle = _CSTD CreateFileW(_Target.generic_wstring().c_str(),
+        static_cast<DWORD>(file_access::readonly | file_access::writeonly), 0, nullptr,
+        static_cast<DWORD>(file_disposition::only_if_exists), static_cast<DWORD>(
+            file_flags::backup_semantics | file_flags::open_reparse_point), nullptr);
+
+    if (_Handle == INVALID_HANDLE_VALUE) { // failed to get handle
+        _Throw_fs_error("failed to get handle", error_type::runtime_error, "remove_junction");
+    }
+
+    unsigned char _Buff[8];
+    reparse_mountpoint_data_buffer& _Reparse_buff = reinterpret_cast<reparse_mountpoint_data_buffer&>(_Buff);
+    unsigned long _Returned; // returned bytes
+
+    // set new information to _Target
+    _CSTD memset(_Buff, 0, sizeof(_Buff));
+    _Reparse_buff._Reparse_tag = static_cast<unsigned long>(file_reparse_tag::mount_point);
+
+    if (!_CSTD DeviceIoControl(_Handle, FSCTL_DELETE_REPARSE_POINT, &_Reparse_buff, 8,
+        nullptr, 0, &_Returned, nullptr) || is_junction(_Target)) { // failed to remove junction
+        _Throw_fs_error("failed to remove junction", error_type::runtime_error, "remove_junction");
+    }
+    _CSTD CloseHandle(_Handle);
+
+    // if won't throw an exception, will be able to return true
+    return true;
 }
 
 // FUNCTION remove_line
@@ -150,4 +242,5 @@ _NODISCARD bool __cdecl rename(const path& _Old, const path& _New) {
 _EXPERIMENTAL_END
 _FILESYSTEM_END
 
+#pragma warning(pop)
 #endif // !_HAS_WINDOWS
